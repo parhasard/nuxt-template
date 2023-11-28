71b8665630018a6050f6a6306f145b276fd608a3
parhasard <hazar_karadag@hotmail.com>
2023-11-28 23:08:06 +0100

    change

--- app.vue
+++ app.vue
@@ -5,10 +5,54 @@
       </NuxtLayout>
     </div>
 </template>
+<script setup>
+import { onMounted, ref } from 'vue';
 
+const scaleFactor = ref(1);
+let initialViewportWidth = null;
+
+// This function checks if the device is probably a mobile
+const isMobileDevice = () => window.innerWidth <= 768;
+
+const adjustScale = () => {
+  const viewportWidth = window.innerWidth;
+
+  if (!initialViewportWidth) {
+    initialViewportWidth = viewportWidth;
+  }
+
+  scaleFactor.value = viewportWidth / initialViewportWidth;
+};
+
+onMounted(() => {
+  if (!isMobileDevice()) {
+    window.addEventListener('resize', adjustScale);
+    adjustScale();
+  }
+});
+</script>
 <style>
 html {
   background-color: black;
+}
+
+.gradient-text-left {
+  background: linear-gradient(45deg, #4bb8a6,#085ba0 );
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+.gradient-text-right {
+  background: linear-gradient(45deg,#085ba0, #4bb8a6 );
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+.gradient-text-1 {
+  background: linear-gradient(45deg,#fafafa, #4bb8a6 );
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
 }
 
 .page-enter-active,
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,58 +1,70 @@
 <template>
-  <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32 ">
+  <div class="relative isolate h-screen overflow-hidden bg-black py-24 sm:py-32 ">
+    <span class="mandala-wrapper">
+  <div id="mandala" class="mandala-container animate-colors animated" aria-hidden="true">
+    <div class="mandala-translate mandala-rotate">
+      <svg width="675" height="675" viewBox="0 0 675 675" fill="none" xmlns="http://www.w3.org/2000/svg" class="mandala">
+        <defs>
+          <path id="singleCircle" d="M337.5,337.5 m-320,0 a320,320 0 1,1 640,0 a320,320 0 1,1 -640,0"></path>
+        </defs>
+        <text class="mandala-ring mandala-accent-1" dy="0" textLength="2010" style="letter-spacing: -2px; word-spacing: 8px;">
+  <textPath href="#singleCircle" style="font-family: 'Comic Sans MS', cursive, sans-serif; font-size: 30px; ">
+    &lt;node pkg="turtlesim" exec="turtlesim_node" name="sim" namespace="turtlesim1"/&gt;
+  </textPath>
+</text>
+<text class="mandala-ring mandala-accent-1" dy="50" textLength="2010" style="letter-spacing: -2px; word-spacing: 8px;">
+  <textPath href="#singleCircle" style="font-family: 'Comic Sans MS', cursive, sans-serif; font-size: 20px; ">
+    
+    ros2 launch &lt;package_name/&gt; &lt;launch_file_name/&gt;
+  </textPath>
+</text>
+<text class="mandala-ring mandala-accent-1" dy="100" textLength="2010" style="letter-spacing: -2px; word-spacing: 8px;">
+  <textPath class="animate-colors2" href="#singleCircle" style="font-family: 'Comic Sans MS', cursive, sans-serif; font-size: 20px; ">
+    [INFO] [launch]: Default logging verbosity is set to INFO
+  </textPath>
+</text>
+<text class="mandala-ring mandala-accent-1" dy="150" textLength="2010" style="letter-spacing: -2px; word-spacing: 8px;">
+  <textPath href="#singleCircle" style="font-family: 'Comic Sans MS', cursive, sans-serif; font-size: 10px; ">
+    &lt;node pkg="turtlesim" exec="mimic" name="mimic"&gt;
+  </textPath>
+</text>
+<text class="mandala-ring mandala-accent-1 " dy="180" textLength="2010" style="letter-spacing: -2px; word-spacing: 8px;">
+  <textPath class="animate-colors1" href="#singleCircle" style="font-family: 'Comic Sans MS', cursive, sans-serif; font-size: 20px;">
+     .                ros2 launch turtlesim_mimic_launch.py                                       .
+  </textPath>
+</text>
 
-    <video v-if="!videoFailed" alt=""
-      class=" absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" id="video" autoplay loop
-      muted playsinline>
-      <source src="/pages/home/automation.mp4" type="video/mp4">
-    </video>
-    <img v-else class=" absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      src="https://images.unsplash.com/photo-1567789884554-0b844b597180?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-      alt="Fallback Image">
-    <div class="video-overlay"></div>
-    <div class="overlay-bottom"></div>
-    <div class="overlay-top"></div>
-    <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
-      aria-hidden="true">
-      <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-        style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-      </div>
+
+
+
+
+      </svg>
     </div>
-    <div
-      class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-      aria-hidden="true">
-      <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-        style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-      </div>
-    </div>
-    <div
-      class="absolute -top-50 left-1 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-2rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-      aria-hidden="true">
-      <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-        style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-      </div>
-    </div>
+  </div>
+</span>
+
     <div class="mx-auto max-w-7xl px-6 lg:px-8" style="z-index: 10;">
       <div class="mx-auto max-w-2xl lg:mx-0">
-        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl ">Be a part of the Roboverse</h2>
-        <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-          lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl gradient-text-left">together, we shape the future of robotics
+        </h2>
+        <p class="mt-6 text-2xl leading-8 text-gray-300">Creating a collaborative platform for robotics enthusiasts,
+          beginners, and professionals to learn, innovate, and share</p>
       </div>
       <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
 
         <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
 
           <div class="flex flex-col-reverse">
-            <dt class="text-base leading-7 text-gray-300">Different projects per each semester</dt>
-            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">4</dd>
+            <dt class="text-base leading-7 text-gray-300">Get Inspired</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">Imagine</dd>
           </div>
           <div class="flex flex-col-reverse">
-            <dt class="text-base leading-7 text-gray-300">Projects till now</dt>
-            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">40+</dd>
+            <dt class="text-base leading-7 text-gray-300">Hone your skils</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">Develop</dd>
           </div>
           <div class="flex flex-col-reverse">
-            <dt class="text-base leading-7 text-gray-300">Personal growth</dt>
-            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">Unlimited</dd>
+            <dt class="text-base leading-7 text-gray-300">Inspire Others</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">Simulate</dd>
           </div>
         </dl>
       </div>
@@ -60,58 +72,87 @@
   </div>
 </template>
 
-<script setup>
-import { onMounted, ref } from 'vue';
+<script setup  lang="ts">
 
-const videoFailed = ref(false);
-
-onMounted(() => {
-  const videoElement = document.getElementById("video");
-  try {
-    videoElement.addEventListener("error", () => {
-      videoFailed.value = true;
-      console.log('sth')
-    });
-
-    videoElement.play().catch(() => {
-      videoFailed.value = true;
-      console.log('sth2')
-    });
-  } catch (error) {
-    console.log('sth terrible happened: ' + error)
-  }
-});
 
 </script>
 
 <style scoped>
-.video-overlay {
+span {
+  display: block;
+  height: 40em;
+  left: calc(50% - 5em);
   position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.7);
-  /* Adjust the opacity as needed */
-  z-index: -9;
+  top: calc(50% - 20em);
+  width: 40em;
 }
 
-.overlay-bottom,
-.overlay-top {
-  position: absolute;
-  left: 0;
-  right: 0;
-  height: 15%;
-  /* Adjust the height as needed */
-  z-index: 1;
+
+.mandala-container {
+  position: relative;
 }
 
-.overlay-bottom {
-  bottom: 0;
-  background: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+.mandala-translate {
+  transform: rotate(0deg); /* Adjust as needed for the initial rotation */
+  transition: transform 10s linear; /* Smooth rotation effect */
 }
 
-.overlay-top {
-  top: 0;
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
-}</style>
+.mandala {
+  height: 100%;
+  width: 100%;
+  
+}
+
+
+
+.mandala-ring {
+  font-family: 'Comic Sans MS', cursive, sans-serif; /* A fun, playful font */
+  font-size: 20px; /* Adjust size as desired */
+  fill: #80757a; /* A bright, attention-grabbing color */
+  opacity: 0.5;
+  
+}
+
+.animate-colors1 {
+  animation: colorChange1 5s infinite alternate;
+}
+.animate-colors2 {
+  animation: colorChange2 5s infinite alternate;
+}
+
+@keyframes colorChange1 {
+  0% { fill: #c6c6c61f; }
+  100% { fill: #7c7781; }
+}
+@keyframes colorChange2 {
+  0% { fill: #F39200; }
+  100% { fill: #E71D73; }
+}
+
+@keyframes spinccw {
+  0% {
+    transform: rotateZ(0);
+  }
+  100% {
+    transform: rotateZ(-360deg);
+  }
+}
+
+@keyframes spincw {
+  0% { transform: rotateZ(0); }
+  100% { transform: rotateZ(360deg); }
+}
+
+.mandala-ring {
+  transform-origin: 337.5px 337.5px;
+}
+
+.mandala-ring:nth-child(odd) {
+  animation: spincw 80s linear infinite;
+}
+
+.mandala-ring:nth-child(even) {
+  animation: spinccw 150s linear infinite;
+}
+
+</style>
